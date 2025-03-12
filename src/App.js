83df8361c3ae2c931c01718318dd28dae5cab710
Vo Minh Thien Long <thienlongtpct@gmail.com>
2025-03-12 17:47:21 +0700

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-import Generator from "./pages/Generator";
-
-export default function App() {
-  return (
-      <Generator />
-  );
-}
--- src/theme/customizations/index.js
+++
@@ -1,5 +0,0 @@
-export { default as inputsCustomizations } from './inputs';
-export { default as dataDisplayCustomizations } from './dataDisplay';
-export { default as feedbackCustomizations } from './feedback';
-export { default as navigationCustomizations } from './navigation.js';
-export { default as surfacesCustomizations } from './surfaces';
